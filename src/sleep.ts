c6c110eb1460b81967160c2382a8eeeceb7ae462
David Wu <55110205+wuwei-hx@users.noreply.github.com>
2021-07-06 11:09:30 +0800

    update type declaration for sleep function

--- src/sleep.ts
+++ src/sleep.ts
@@ -2,6 +2,6 @@
  * 休眠指定时间
  * @param ms 休眠毫秒数
  */
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
 
 export default sleep;
